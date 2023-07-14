e2f461dbbe74ac973180cb28809ed9eae2307583
WillyTALOM <103586241+WillyTALOM@users.noreply.github.com>
2023-07-14 12:59:53 +0200

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -14,6 +14,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use App\Form\AdressType;
 use Rollerworks\Component\PasswordStrength\Validator\Constraints\PasswordStrength;
 
 
